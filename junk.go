27924e1512cfff0b618b8facf5d3d4283b44aca9
saurabh98s <geek.saurabh98@gmail.com>
2019-02-07 13:09:48 +0530

    Create junk.go

---
+++ junk.go
@@ -0,0 +1,36 @@
+package main
+import ( "fmt"
+		 "database/sql"
+		 _"github.com/go-sql-driver/mysql"
+		 )
+func main() {
+	fmt.Println("BEGINNNG DB CONNECTION!")
+
+	db,err := sql.Open("mysql","root:czlq5hvu@tcp(127.0.0.1)/mydb") //define database and error
+	if err!=nil{
+		panic(err.Error()) //panic prints the error.
+	}
+	fmt.Println("Connected")
+	rows,err := db.Query("SELECT * FROM names")
+	checkErr(err)
+	for rows.Next(){
+
+		var LastName string //define values
+		var FirstName string
+		var RegNo int
+		var BRANCH string
+		err = rows.Scan(&LastName, &FirstName, &RegNo, &BRANCH) //read values
+		checkErr(err)
+		fmt.Println("LAST NAME:",LastName)
+		fmt.Println("FIRST NAME",FirstName)
+		fmt.Println("REG NO:",RegNo)
+		fmt.Println("BRANCH:",BRANCH)
+	}
+	defer db.Close()
+}
+func checkErr(err error) {
+
+	if err!=nil { //check errors
+		panic(err)
+	}
+}
