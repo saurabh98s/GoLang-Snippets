4f62cf817cfe78369f1ea8b196d4027a65f1460c
saurabh98s <geek.saurabh98@gmail.com>
2020-04-13 13:56:00 +0530

    created the login logic

--- TEMPLATES/sessions_with_go/main.go
+++ TEMPLATES/sessions_with_go/main.go
@@ -21,11 +21,16 @@ var dbSessions = map[string]string{} //sessionid,user id
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+	// to implement the logic logic we need to have a pre-existing user
+	// since there is no DB connection yet,we will create one and store it in the map
+	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	dbUser["test@test.com"] = user{"test@test.com", bs, "James", "Bond"}
 }
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/bar", bar)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 
@@ -36,44 +41,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.html", u)
 
 }
-
-// func foo(w http.ResponseWriter, r *http.Request) {
-
-// 	// get cookie
-// 	c, err := r.Cookie("session")
-// 	if err != nil {
-// 		sID, _ := uuid.NewV4()
-// 		c = &http.Cookie{
-
-// 			Name:  "session",
-// 			Value: sID.String(),
-// 		}
-// 		http.SetCookie(w, c) //writing cookie into the session
-// 	}
-
-// 	// if user exists,get user
-
-// 	var u user
-
-// 	if un, ok := dbSessions[c.Value]; ok {
-// 		u = dbUser[un]
-// 	}
-
-// 	// form submission
-// 	if r.Method == http.MethodPost {
-// 		un := r.FormValue("username")
-// 		p := r.FormValue("password")
-// 		f := r.FormValue("firstname")
-// 		l := r.FormValue("lastname")
-// 		u = user{un, p, f, l}
-// 		dbSessions[c.Value] = un //assigning user a uuid
-// 		dbUser[un] = u
-// 	}
-
-// 	// if get method
-// 	tpl.ExecuteTemplate(w, "index.html", u)
-
-// }
 
 func bar(w http.ResponseWriter, r *http.Request) {
 	u := getUser(w, r)
@@ -129,5 +96,43 @@ func signup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tpl.ExecuteTemplate(w, "signup.html", nil)
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if alreadyLoggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// process form submission
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		u, ok := dbUser[un]
+		if !ok {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		// does the entered password match the stored password?
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "Username and/or password do not match", http.StatusSeeOther)
+			return
+		}
+
+		//create session
+
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "login.html", nil)
 
 }
