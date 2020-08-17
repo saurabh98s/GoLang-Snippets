beb758afe343a04e0e382d42188c21ee32f56a04
saurabh98s <geek.saurabh98@gmail.com>
2020-08-18 03:28:00 +0530

    adding context to client

--- context_with_go/client/main.go
+++ context_with_go/client/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"log"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:8080")
+	ctx := context.Background()
+	ctx,cancel :=context.WithTimeout(ctx,time.Second)
+	defer cancel()
+	req,err:=http.NewRequest(http.MethodGet,"http://localhost:8080",nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req=req.WithContext(ctx)
+	resp,err:= http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
